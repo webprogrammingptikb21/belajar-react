2e50cfd9b302ec7d3813ca71fae96e569431c7a8
Alwiii <210209501011@student.unm.ac.id>
2024-11-07 17:58:50 +0800

    belajar react

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+const groceryItems = [
+  {
+    id: 1,
+    name: "Kopi bubuk",
+    quantity: 5,
+    checked: true,
+  },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Form />
+      <GroceryList />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Catatan Belanjaku 📝</h1>;
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>Hari ini belanja apa kita?</h3>
+      <div>
+        <select>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <input type="text" placeholder="nama barang..." />
+      </div>
+      <button>Tambah</button>
+    </form>
+  );
+}
+
+function GroceryList() {
+  return (
+    <>
+      <div className="list">
+        <ul>
+          <li>
+            <input type="checkbox" checked="true" />
+            <span style="text-decoration: line-through;">1 Kopi</span>
+            <button>&times;</button>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <span>5 Gula Pasir</span>
+            <button>&times;</button>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <span>3 Air Mineral</span>
+            <button>&times;</button>
+          </li>
+        </ul>
+      </div>
+      <div className="actions">
+        <select>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button>Bersihkan Daftar</button>
+      </div>
+    </>
+  );
+}
+
+function Footer() {
+  return <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>;
+}
