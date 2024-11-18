3f16b44d664cc3184ae1275ec62069753e5674a4
Alwiii <210209501011@student.unm.ac.id>
2024-11-19 01:07:19 +0800

    menambahkan komponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import GroceryList from "./components/GrowceryList";
+import Footer from "./components/Footer";
 const groceryItems = [
   {
     id: 1,
@@ -45,117 +49,7 @@ export default function App() {
       <Header />
       <Form onAddItem={handleAddItem} />
       <GroceryList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleCheckItem} onClearItems={handleClearItems} />
-      <Footer />
+      <Footer items={items} />
     </div>
   );
-}
-
-function Header() {
-  return <h1>Catatan Belanjaku 📝</h1>;
-}
-
-function Form({ onAddItem }) {
-  const [name, setName] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!name) return;
-
-    const newItem = {
-      name: name,
-      quantity: quantity,
-      checked: false,
-      id: Date.now(),
-    };
-    onAddItem(newItem);
-    console.log(newItem);
-
-    setName("");
-    setQuantity(1);
-  }
-
-  const quantityNum = [...Array(20)].map((_, i) => (
-    <option key={i + 1} value={i + 1}>
-      {i + 1}
-    </option>
-  ));
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>Hari ini belanja apa kita?</h3>
-      <div>
-        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-          {quantityNum}
-        </select>
-        <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <button>Tambah</button>
-    </form>
-  );
-}
-
-function GroceryList({ items, onDeleteItem, onToggleItem, onClearItems }) {
-  const [sortBy, setSortBy] = useState("input");
-
-  let sortedItems;
-
-  if (sortBy === "input") {
-    //jika sortby nya itu isinya input
-    sortedItems = items; //sorteditems akan berisi items
-  }
-
-  if (sortBy == "name") {
-    sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
-  }
-
-  if (sortBy === "checked") {
-    sortedItems = items.slice().sort((a, b) => a.checked - b.checked);
-  }
-  // switch (sortBy) {
-  //   case "name":
-  //     sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
-  //     break;
-  //   case "checked":
-  //     sortedItems = items.slice().sort((a, b) => a.checked - b.checked);
-  //     break;
-  //   default:
-  //     sortedItems = items;
-  //     break;
-  // }
-  return (
-    <>
-      <div className="list">
-        <ul>
-          {sortedItems.map((item) => (
-            <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
-          ))}
-        </ul>
-      </div>
-      <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">Urutkan berdasarkan urutan input</option>
-          <option value="name">Urutkan berdasarkan nama barang</option>
-          <option value="checked">Urutkan berdasarkan ceklis</option>
-        </select>
-        <button onClick={onClearItems}>Bersihkan Daftar</button>
-      </div>
-    </>
-  );
-}
-
-function Item({ item, onDeleteItem, onToggleItem }) {
-  return (
-    <li key={item.id}>
-      <input type="checkbox" checked={item.checked} onChange={() => onToggleItem(item.id)} />
-      <span style={item.checked ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.name}
-      </span>
-      <button onClick={() => onDeleteItem(item.id)}>&times;</button>
-    </li>
-  );
-}
-
-function Footer() {
-  return <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>;
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+export default function Footer({ items }) {
+  if (items.length === 0) return <footer className="stats">Daftar belanjaan masih kosong</footer>;
+
+  const totalItems = items.length;
+  const checkedItems = items.filter((item) => item.checked).length;
+  const percentage = Math.round((checkedItems / totalItems) * 100);
+  return (
+    <footer className="stats">
+      Ada {totalItems} barang di daftar belanjaan, {checkedItems} barang sudah dibeli ({percentage}%)
+    </footer>
+  );
+}
